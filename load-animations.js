d4c82816c440c805b0970b954906ac9aee2ddcb2
Mark Kuznetsov <mkuz.fl@gmail.com>
2023-08-11 21:42:58 -0600

    v1.2 added more stuff

--- load-animations.js
+++ load-animations.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const bannerText = document.getElementsByClassName('imgbanner-title')[0]
     bannerText.classList.add('active')
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const midTitle = document.getElementsByClassName('mid-text-title')[0]
+    midTitle.classList.add('active')
+})
